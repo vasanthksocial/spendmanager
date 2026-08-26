d8fef8c75a2deefa15f322b6286d411f04978bf3
vasanthksocial <vasanthksocial@gmail.com>
2026-08-26 15:08:50 +0530

    Add RETURN and RESUBMIT to ApprovalAction enum
    Added RETURN and RESUBMIT actions to ApprovalAction enum.

--- src/main/java/com/dept/spendmgmt/model/ApprovalAction.java
+++ src/main/java/com/dept/spendmgmt/model/ApprovalAction.java
@@ -3,5 +3,7 @@ package com.dept.spendmgmt.model;
 public enum ApprovalAction {
     APPROVE,
     REJECT,
-    MODIFY
+    MODIFY,
+    RETURN,   // approver sends the invoice back to the submitter for correction (not terminal)
+    RESUBMIT  // submitter corrects and resubmits after a RETURN
 }
