a9c63bb2d25c7628ec70ec13203c3f35a09606a3
vasanthksocial <vasanthksocial@gmail.com>
2026-08-26 15:40:15 +0530

    Update AppUserRepository.java

--- src/main/java/com/dept/spendmgmt/repository/AppUserRepository.java
+++ src/main/java/com/dept/spendmgmt/repository/AppUserRepository.java
@@ -1,7 +1,12 @@
 package com.dept.spendmgmt.repository;
 
 import com.dept.spendmgmt.model.AppUser;
+import com.dept.spendmgmt.model.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+    List<AppUser> findByRoleAndDivisionOfficeId(UserRole role, Long divisionOfficeId);
+    List<AppUser> findByRole(UserRole role);
 }
