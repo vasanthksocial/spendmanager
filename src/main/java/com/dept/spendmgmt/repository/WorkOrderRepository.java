82927e2449652dd0596b3db23cad289edb841943
vasanthksocial <vasanthksocial@gmail.com>
2026-08-27 11:47:57 +0530

    Update WorkOrderRepository.java

--- src/main/java/com/dept/spendmgmt/repository/WorkOrderRepository.java
+++ src/main/java/com/dept/spendmgmt/repository/WorkOrderRepository.java
@@ -3,5 +3,8 @@ package com.dept.spendmgmt.repository;
 import com.dept.spendmgmt.model.WorkOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WorkOrderRepository extends JpaRepository<WorkOrder, Long> {
+    List<WorkOrder> findByDivisionOfficeId(Long divisionOfficeId);
 }
